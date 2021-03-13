742c78ece590bc8ee11d61e88c36a3305927b784
tchartchke <chartai.kl@gmail.com>
2021-03-13 13:07:23 -0600

    creating quizzes

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -3,18 +3,37 @@ class QuizzesController < ApplicationController
 
   def index
     #shows all quizzes current user has created - has link to make new quiz
-    
+    # raise current_user.quizzes.inspect
+
+    #find all quizzes that belong to user of current session
+    @quizzes = current_user.quizzes
   end
 
   def show
     #shows individual quiz and links to let edit and delete
+    #how to belong to user
+    @quiz = Quiz.find(params[:id])
+
+    redirect_to '/home' unless @quiz.host == current_user
   end
 
   def new
-    #createa a new quiz
+    @user = current_user
+    @quiz = Quiz.new(host_id:@user.id)
   end
 
   def create
-    # 
+    @quiz = Quiz.new(quiz_params)
+    if @quiz.save
+      redirect_to user_quiz_path(current_user, @quiz)
+    else
+      redirect_to new_user_quiz_path(current_user)
+    end
   end
+  
+  private
+
+  def quiz_params
+    params.require(:quiz).permit(:title, :host_id)
+  end 
 end
--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -2,4 +2,6 @@ class Quiz < ApplicationRecord
   belongs_to :host, :class_name => 'User'
   has_many :rounds
   has_many :questions, through: :rounds
+
+  validates :title, presence: true
 end
